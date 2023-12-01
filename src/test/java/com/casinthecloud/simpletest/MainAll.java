43dd23405a877a1a92fa518e6603d5e401b92cef
Jérôme LELEU <leleuj@gmail.com>
2023-12-01 17:23:02 +0100

    all tests OK

--- src/test/java/com/casinthecloud/simpletest/MainAll.java
+++ src/test/java/com/casinthecloud/simpletest/MainAll.java
@@ -17,5 +17,6 @@ public class MainAll {
 
         MainSAMLDelegateCAS.main(args);
         MainSAMLDelegateOIDC.main(args);
+        MainSAMLDelegateSAML.main(args);
     }
 }
--- src/test/java/com/casinthecloud/simpletest/MainSAMLDelegateSAML.java
+++ src/test/java/com/casinthecloud/simpletest/MainSAMLDelegateSAML.java
@@ -3,15 +3,12 @@ package com.casinthecloud.simpletest;
 import com.casinthecloud.simpletest.cas.CasDelegate;
 import com.casinthecloud.simpletest.cas.CasSAML2Login;
 import com.casinthecloud.simpletest.execution.Execution;
-import lombok.val;
 
 public class MainSAMLDelegateSAML {
 
     public static void main(final String... args) throws Exception {
         new Execution(() -> {
-            val login = new CasSAML2Login(new CasDelegate(2, "SAML2Client", new CasSAML2Login()));
-            login.setCasPrefixUrl("http://oidc-server:8080/cas");
-            return login;
+            return new CasSAML2Login(new CasDelegate(1, "SAML2Client", new CasSAML2Login()));
         }).launch();
     }
 }
